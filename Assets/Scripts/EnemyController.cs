deac33f2300e15013e3f7862b9966b84873e66f8
Laura Mambrilla Moreno <lauramambrilla@gmail.com>
2024-02-14 19:49:30 +0100

    MissileEnemy and MisilePlayer prefabs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float xPositionDestroy;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Destroy();
+    }
+
+    private void Move()
+    {
+        transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+    }
+
+    private void Destroy()
+    {
+        if (transform.position.x >= xPositionDestroy)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
